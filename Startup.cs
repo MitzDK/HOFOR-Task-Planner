984d090d8d1d001f4857744b2289df9c494f9ea9
MitzDK <victorwilliamjejlskov@gmail.com>
2022-04-28 13:18:23 +0200

    Installed 4 packages Entityframework stuff x3 RuntimeCompilation
    Added 1 line to Startup to allow RuntimeCompil to work.

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace HOFORTaskPlanner
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
