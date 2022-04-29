85c908dff5567682b4af1572670e8995ec7133ea
MitzDK <victorwilliamjejlskov@gmail.com>
2022-04-29 11:23:49 +0200

    Added Entity Framework Annotations for Aktion model

--- Models/Aktion.cs
+++ Models/Aktion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,69 @@ namespace HOFORTaskPlanner.Models
 {
     public class Aktion
     {
+        public enum UserTypes
+        {
+            Admin,
+            User
+        }
+
+        public enum UserRoles
+        {
+            Implementering,
+            Tovholder,
+            Projektleder,
+            D365Platform,
+            D365Ce,
+            D365Fo,
+            Udvikler,
+            Koordinator,
+            Bi,
+            Web,
+            Isdm,
+            Løsningsdesign,
+            Klient,
+            Netværk,
+            Server,
+            Azure,
+            Sikkerhed,
+            Dba,
+            Lcm,
+            Support,
+            Støttefunktion,
+            Leder,
+            Xprojektleder
+        }
+
+        public enum UserDepartments
+        {
+            Implementering,
+            Udvikling,
+            Infrastruktur,
+            Digitalisering,
+            Service
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+        [Required] 
+        public string Username { get; set; }
+        [Required][DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        public UserTypes UserType { get; set; }
+        [Required]
+        public UserRoles UserRole { get; set; }
+        [Required]
+        public UserDepartments UserDepartment { get; set; }
+        [Required]
+        public DateTime LastUpdated { get; set; }
+
+
     }
 }
