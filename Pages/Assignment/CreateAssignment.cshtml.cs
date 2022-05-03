9b3c8405892493136c5bf2eb5416179d7f4dc5ec
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2022-05-03 13:31:46 +0200

    Deployed usage of AssignmentService in CreateAssignment.cshtml.cs

--- Pages/Assignment/CreateAssignment.cshtml.cs
+++ Pages/Assignment/CreateAssignment.cshtml.cs
@@ -1,6 +1,7 @@
 using System;using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HOFORTaskPlanner.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,32 +9,32 @@ namespace HOFORTaskPlanner.Pages.Assignment
 {
     public class CreateAssignmentModel : PageModel
     {
-        //private AssignmentService _assignmentService;
+        private AssignmentService _assignmentService;
         private List<Models.Assignment> _assignments;
 
         [BindProperty] public Models.Assignment Assignment { get; set; }
 
-        //public CreateAssignmentModel(AssignmentService assignmentService)
-        //{
-            // _assignmentService = assignmentService;
-            // _assignments = _assignmentService.GetAssignments().ToList();
-        //}
+        public CreateAssignmentModel(AssignmentService assignmentService)
+        {
+            _assignmentService = assignmentService;
+            _assignments = _assignmentService.GetAssignments().ToList();
+        }
 
-        
+
 
         public void OnGet()
         {
         }
 
-        //public IActionResult OnPost()
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return Page();
-        //    }
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
-        //    _AssignmentService.AddAssignmentAsync(Assignment);
-        //    return RedirectToPage("Index");
-        //}
+            await _assignmentService.AddAssignmentAsync(Assignment);
+            return RedirectToPage("Index");
+        }
     }
 }
