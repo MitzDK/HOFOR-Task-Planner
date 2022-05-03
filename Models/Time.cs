1d3d5e82e807202a3ed4f68462e42c13a3933ea5
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2022-05-03 11:01:56 +0200

    Added Time model.

---
+++ Models/Time.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace HOFORTaskPlanner.Models
+{
+    public class Time
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+
+        public int Hours { get; set; }
+        //public User User { get; set; }
+        //public Assignment Assignment { get; set; )
+
+        public Time()
+        {
+            Year = DateTime.Now.Year;
+            Month = DateTime.Now.Month;
+        }
+
+        //public Time(User user, Assignment assignment)
+        //{
+        //    Year = DateTime.Now.Year;
+        //    Month = DateTime.Now.Month;
+        //    User = user;
+        //    Assignment = assignment;
+        //}
+    }
+}
