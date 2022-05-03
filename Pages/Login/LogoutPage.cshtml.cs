063b2f769969b5145ffb6d9d2070d1b37e8f3f02
AndreasBroberg <90028853+AndreasBroberg@users.noreply.github.com>
2022-05-03 14:48:57 +0200

    Login Logout Account - Access denied UserService Layout login og logout

--- Pages/Login/LogoutPage.cshtml.cs
+++ Pages/Login/LogoutPage.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,12 @@ namespace HOFORTaskPlanner.Pages.Login
 {
     public class LogoutPageModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            LoginPageModel.LoggedInUser = null;
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Index");
         }
     }
 }
