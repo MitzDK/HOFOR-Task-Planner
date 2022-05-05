fc47c7a68f0494a289706d073450a36ce010f741
MitzDK <victorwilliamjejlskov@gmail.com>
2022-05-05 12:51:43 +0200

    Tilføjet dropdown til Create Assignment, samt ændret lidt i GetAssignment designet

--- Pages/Assignment/CreateAssignment.cshtml.cs
+++ Pages/Assignment/CreateAssignment.cshtml.cs
@@ -1,4 +1,6 @@
-using System;using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using HOFORTaskPlanner.Pages.Login;
@@ -13,16 +15,24 @@ namespace HOFORTaskPlanner.Pages.Assignment
         private AssignmentService _assignmentService;
 
         [BindProperty] public Models.Assignment Assignment { get; set; }
+        private UserService _userService;
+        public List<Models.User> Users { get; set; }
+        [Display(Name = "Aktion til opgaven")]
+        [BindProperty] public string AktionSearch { get; set; }
+        [Display(Name = "Styring til opgaven")]
+        [BindProperty] public string ControllerSearch { get; set; }
 
-        public CreateAssignmentModel(AssignmentService assignmentService)
+        public CreateAssignmentModel(AssignmentService assignmentService, UserService userService)
         {
             _assignmentService = assignmentService;
+            _userService = userService;
         }
 
 
 
         public void OnGet()
         {
+            Users = _userService.GetUsers();
         }
 
         public async Task<IActionResult> OnPost()
@@ -32,8 +42,23 @@ namespace HOFORTaskPlanner.Pages.Assignment
                 return Page();
             }
 
-            Assignment.AktionUserId = LoginPageModel.LoggedInUser.UserId;
-            Assignment.ControlUserId = LoginPageModel.LoggedInUser.UserId;
+            if (_userService.GetUserByDisplayName(AktionSearch) != null)
+            {
+                Assignment.AktionUserId = _userService.GetUserByDisplayName(AktionSearch).UserId;
+            }
+            else
+            {
+                Assignment.AktionUserId = 0;
+            }
+            if (_userService.GetUserByDisplayName(ControllerSearch) != null)
+            {
+                Assignment.ControlUserId = _userService.GetUserByDisplayName(ControllerSearch).UserId;
+            }
+            else
+            {
+                Assignment.ControlUserId = 0;
+            }
+
             await _assignmentService.AddAssignmentAsync(Assignment);
             return RedirectToPage("../Index");
         }
