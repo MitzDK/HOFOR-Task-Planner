69ebb250aa773e29212440f2bafbb67f6c245d2a
Slagtarn <94110452+Slagtarn@users.noreply.github.com>
2022-05-05 14:04:39 +0200

    EditUser - GetUser - Layoutændring (Ændret til Bootstrap 3) - GetUsers tilrettet - Modelklassen Time har fået tilføjet enum.

---
+++ Pages/Admin/EditUser.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HOFORTaskPlanner.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HOFORTaskPlanner.Pages.Admin
+{
+    public class EditUserModel : PageModel
+    {
+        private UserService _userService;
+        [BindProperty] public Models.User.UserDepartments UserDepartments { get; set; }
+        [BindProperty] public Models.User.UserRoles UserRoles { get; set; }
+        [BindProperty] public string FirstName { get; set; }
+        [BindProperty] public string LastName { get; set; }
+        [BindProperty] public string DisplayName { get; set; }
+
+
+        public EditUserModel(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            var user = _userService.GetUserById(id);
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            DisplayName = user.DisplayName;
+            UserDepartments = user.UserDepartment;
+            UserRoles = user.UserRole;
+            if (User == null)
+            {
+                return RedirectToPage("../Index"); //Alternativt oprettes ny side, som i Itemrazor
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost(int id)
+        {
+            var user = _userService.GetUserById(id);
+            user.FirstName = FirstName;
+            user.LastName = LastName;
+            user.DisplayName = DisplayName;
+            user.UserRole = UserRoles;
+            user.UserDepartment = UserDepartments;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _userService.UpdateUserAsync(user);
+            return RedirectToPage("../User/GetUsers");
+        }
+    }
+}
