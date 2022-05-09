a9bcfcef4d4e445972601fd04b65a023d6cab2ff
Slagtarn <94110452+Slagtarn@users.noreply.github.com>
2022-05-09 11:39:52 +0200

    DeleteUser/ArkiverUser er lavet

--- Pages/Admin/DeleteUser.cshtml.cs
+++ Pages/Admin/DeleteUser.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HOFORTaskPlanner.MockData;
 using HOFORTaskPlanner.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,15 +15,25 @@ namespace HOFORTaskPlanner.Pages.Admin
     {
         private UserService _userService;
         public Models.User User { get; set; }
+        public List<Models.User> Users { get; set; }
 
         public DeleteUserModel(UserService userService)
         {
             _userService = userService;
+            Users = _userService.GetUsers();
         }
 
         public void OnGet(int id)
         {
             User = _userService.GetUserById(id);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            User.Password = "Arkiveretbruger";
+            User.UserType = Models.User.UserTypes.Arkiveret;
+            await _userService.UpdateUserAsync(User);
+            return RedirectToPage("../User/GetUsers");
         }
     }
 }
