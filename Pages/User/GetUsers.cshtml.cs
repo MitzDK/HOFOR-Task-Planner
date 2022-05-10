ec6a3eb40df42568f0a6ab78fffcff2aa01075cb
Slagtarn <94110452+Slagtarn@users.noreply.github.com>
2022-05-10 18:49:22 +0200

    PagePagination - Users fikset. Opret assignment tilføjet

--- Pages/User/GetUsers.cshtml.cs
+++ Pages/User/GetUsers.cshtml.cs
@@ -13,22 +13,35 @@ namespace HOFORTaskPlanner.Pages.User
         public IEnumerable<Models.User> UserList;
         private UserService _userService;
         [BindProperty] public Models.User.UserDepartments UserDepartments { get; set; }
-
+        [BindProperty(SupportsGet = true)] public int CurrentPage { get; set; } = 1;
+        public int Count { get; set; }
+        public int PageSize { get; set; } = 10;
+        public int TotalPages => (int)Math.Ceiling(decimal.Divide(Count, PageSize));
         public Models.User User { get; set; }
 
         public GetUsersModel(UserService userService)
         {
             _userService = userService;
         }
-        public void OnGet()
-        {
-            UserList = _userService.GetUsers();
-        }
+        //public void OnGet()
+        //{
+        //    UserList = _userService.GetUsers();
+        //}
 
         public IActionResult OnPost()
         {
             UserList = _userService.FilterTeams(UserDepartments);
             return Page();
+        }
+        public bool ShowPrevious => CurrentPage > 1;
+        public bool ShowNext => CurrentPage < TotalPages;
+        public bool ShowFirst => CurrentPage != 1;
+        public bool ShowLast => CurrentPage != TotalPages;
+
+        public void OnGet()
+        {
+            UserList = _userService.GetPaginatedResult(CurrentPage, PageSize);
+            Count = _userService.GetCount();
         }
     }
 }
