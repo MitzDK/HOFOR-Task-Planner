1215d5399d70ad2b15da78892046b20e32f0aab8
Slagtarn <94110452+Slagtarn@users.noreply.github.com>
2022-05-10 16:29:44 +0200

    123

--- Pages/Assignment/GetAssignments.cshtml.cs
+++ Pages/Assignment/GetAssignments.cshtml.cs
@@ -14,6 +14,11 @@ namespace HOFORTaskPlanner.Pages.Assignment
         private AssignmentService _assignmentService;
         private UserService _userService;
         private ContactService _contactService;
+        [BindProperty(SupportsGet = true)] public int CurrentPage { get; set; } = 1;
+        public int Count { get; set; }
+        public int PageSize { get; set; } = 10;
+        public int TotalPages => (int)Math.Ceiling(decimal.Divide(Count, PageSize));
+
 
         public Models.User AssignmentUser(int userId)
         {
@@ -42,9 +47,19 @@ namespace HOFORTaskPlanner.Pages.Assignment
             _userService = userService;
             _contactService = contactService;
         }
-        public void OnGet()
+        //public void OnGet()
+        //{
+        //    AssignmentList = _assignmentService.GetAssignments();
+        //}
+        public bool ShowPrevious => CurrentPage > 1;
+        public bool ShowNext => CurrentPage < TotalPages;
+        public bool ShowFirst => CurrentPage != 1;
+        public bool ShowLast => CurrentPage != TotalPages;
+
+        public async Task OnGetAsync()
         {
-            AssignmentList = _assignmentService.GetAssignments();
+            AssignmentList = await _assignmentService.GetPaginatedResult(CurrentPage, PageSize);
+            Count = await _assignmentService.GetCount();
         }
     }
 }
