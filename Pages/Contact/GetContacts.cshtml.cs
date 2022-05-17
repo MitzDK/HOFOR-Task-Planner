05392e79ecd3fcd8777f671533c5a2eed7ed865b
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2022-05-17 11:47:43 +0200

    Added pagination for GetContacts

--- Pages/Contact/GetContacts.cshtml.cs
+++ Pages/Contact/GetContacts.cshtml.cs
@@ -12,6 +12,15 @@ namespace HOFORTaskPlanner.Pages.Contact
     {
         public List<Models.Contact> Contacts { get; set; }
         private ContactService _contactService;
+        [BindProperty(SupportsGet = true)] public int CurrentPage { get; set; } = 1;
+        public int Count { get; set; }
+        public int PageSize { get; set; } = 10;
+        public int TotalPages =>(int)Math.Ceiling(decimal.Divide(Count, PageSize));
+
+        public bool ShowPrevious => CurrentPage > 1;
+        public bool ShowNext => CurrentPage < TotalPages;
+        public bool ShowFirst => CurrentPage != 1;
+        public bool ShowLast => CurrentPage != TotalPages;
 
         public GetContactsModel(ContactService contactService)
         {
@@ -20,7 +29,18 @@ namespace HOFORTaskPlanner.Pages.Contact
 
         public void OnGet()
         {
-            Contacts = _contactService.GetContacts();
+            Contacts = _contactService.GetPaginatedResultList(_contactService.GetContacts(), CurrentPage, PageSize);
+            Count = _contactService.GetContacts().Count();
+        }
+
+        public IActionResult OnPost()
+        {
+            CurrentPage = 1;
+            Contacts = _contactService.GetPaginatedResultList(_contactService.GetContacts(), CurrentPage, PageSize);
+            Count = _contactService.GetContacts().Count();
+
+
+            return Page();
         }
     }
 }
