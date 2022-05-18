7b22777f8f467c5bcac5910c0d0167cdf371e57d
MitzDK <victorwilliamjejlskov@gmail.com>
2022-05-18 15:28:29 +0200

    Tilføjet lidt doku

--- Pages/Assignment/TimePlanning.cshtml.cs
+++ Pages/Assignment/TimePlanning.cshtml.cs
@@ -38,11 +38,13 @@ namespace HOFORTaskPlanner.Pages.Assignment
             await FinishUpdatingAsync();
             return RedirectToPage("GetAssignments");
         }
-
+        //Returnerer et tal for hvor mange timer der i alt er planlagt for en opgave, bruges hovedsageligt til at udregne "rest"
         public int TotalTimer(int id)
         {
             return TimeService.TotalTimeForAssignmentPlanned(id);
         }
+        //Bruges som en "failsafe", burde aldrig returnere null, men bruges til at få fat i en allerede eksisterende liste af "Time"
+        //For en gældende opgave og år
         public List<TimeReg> ShowList(int year, int id)
         {
             List<TimeReg> newList = new List<TimeReg>();
@@ -53,16 +55,19 @@ namespace HOFORTaskPlanner.Pages.Assignment
             }
             return null;
         }
+        //Et kald ind i timeservicen, som tjekker om det input stemmer overens med Datetime.Now.Month
         public bool IsCurrentMonth(int input)
         {
             return TimeService.IsCurrentMonth(input);
         }
+        //En simpel metode, som returnerer en string, der bruges til at sætte "class" inde i html koden - Her bruges den til den røde linje i bunden af tabbellen
         public string GetClassForCurrentMonth(bool isLast)
         {
             if (isLast) return "IsCurrentMonthBottom";
             return "IsCurrentMonthMiddle";
         }
-
+        //Endnu en lidt eksperimentel metode.. Først tjekkes der om nogle "Time" allerede eksisterer i databasen til den gældende opgave, dernæst fyldes der en masse 0'er på 
+        //de steder hvor der ikke allerede findes en tid
         public List<int> PopulateTable(Models.Assignment assignment)
         {
             var newlist = new List<int>();
@@ -85,7 +90,8 @@ namespace HOFORTaskPlanner.Pages.Assignment
             }
             return newlist;
         }
-
+        //Lidt eksperimentel metode.. Den opretter en liste, og går så igennem vores TimePlanning fra punkt til prikke.
+        //Den tjekker for hver "Time", om den allerede eksisterer, hvis den gør, hentes den fra databasen og smides i listen, hvis den ikke gør, bliver der oprettet en ny.
         public async Task FinishUpdatingAsync()
         {
             var newList = new List<TimeReg>();
@@ -114,20 +120,24 @@ namespace HOFORTaskPlanner.Pages.Assignment
             }
             await TimeService.AddAndUpdateTimes(newList);
         }
+        //Hopper ind i vores TimeService og tjekker hvor vidt den int vi smider med er lig med Datetime.Now.Year
         public bool IsCurrentYear(int input)
         {
             return TimeService.IsCurrentYear(input);
         }
+        //En ikke super fin metode, som bruges til at returnere en værdi, som direkte ændrer stylen i vores TimePlanning tabel
         public string GetClassForCurrentYear(int i)
         {
             if (TimeService.IsCurrentYear(i)) return "background-color: lightgray";
             return "";
         }
+        //En ikke super fin metode, som bruges til at returnere en værdi, som direkte ændrer stylen i vores TimePlanning tabel
         public string GetClassForCurrentYearFirst(int i)
         {    
             if (TimeService.IsCurrentYear(i)) return "background-color: lightgray";
             return "border-right: 2px black;";
         }
+        //Bruges til at teste datoen, som vi videre bruger til at beslutte om man skal kunne indtaste en værdi i TimePlanning eller ej
         public DateTime DateChecker(int year, int month)
         {
             return new DateTime(year, month, 1);
